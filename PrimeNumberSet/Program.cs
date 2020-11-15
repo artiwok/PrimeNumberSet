f28b11cffe7fb8b3432c32b0f46edbb9b10eb583
artiwok <artemolga@ukr.net>
2020-11-15 23:44:01 +0200

    Add project files.

---
+++ PrimeNumberSet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberSet
+{
+    class Program
+    {
+        static bool PrimeNumberSet(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+
+            int val = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The prime numbers' set is: ");
+
+            for (int i = 0; i < val; i++)
+            {
+                if (PrimeNumberSet(i))
+                {
+                    Console.WriteLine($"\t\t\t\t{i}\n");
+                }
+            }
+        }
+    }
+}
